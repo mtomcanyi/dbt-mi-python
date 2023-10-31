b8a3aa51c13c2c4eab59e3ee1ce2442bf3686e3a
Michal Tomcanyi <mtomcanyi@gmail.com>
2023-10-31 18:08:50 +0100

    UPDATE: Initialize profile from workspace variables

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
+import json
 from dbt.cli.main import dbtRunner, dbtRunnerResult
+
+
+    
+
 
 dbt = dbtRunner()
 
@@ -15,14 +20,25 @@ repository_path = "/data/repository"
 work_dir = f"{repository_path}/mi_postgres"
 os.chdir(work_dir)
 
+# Parse config
+with open('/data/config.json') as f:
+    json_data = json.load(f)
+    DB_HOST = json_data['parameters']['DB_HOST']
+    DB_PORT= json_data['parameters']['DB_PORT']
+    DB_NAME = json_data['parameters']['DB_NAME']
+    DB_USER = json_data['parameters']['DB_USER']
+    DB_PASS = json_data['parameters']['DB_PASS']
+    DB_SCHEMA = json_data['parameters']['DB_SCHEMA']
+
+
 
 # Initialize environment variables
-os.environ["DBT_DEV_HOST"] = "changeme.meiro.io"
-os.environ["DBT_DEV_PORT"] = "5432"
-os.environ["DBT_DEV_USER"] = "changeme"
-os.environ["DBT_DEV_PASS"] = "changeme"
-os.environ["DBT_DEV_DBNAME"] = "changeme"
-os.environ["DBT_DEV_SCHEMA"] = "changeme"
+os.environ["DBT_DEV_HOST"] = DB_HOST
+os.environ["DBT_DEV_PORT"] = DB_PORT
+os.environ["DBT_DEV_USER"] = DB_USER
+os.environ["DBT_DEV_PASS"] = DB_PASS
+os.environ["DBT_DEV_DBNAME"] = DB_NAME
+os.environ["DBT_DEV_SCHEMA"] = DB_SCHEMA
 
 
 res: dbtRunnerResult = dbt.invoke(cli_args)
