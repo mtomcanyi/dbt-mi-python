f40ea02c30dc994d7dc59a3e3756bd31a81ca08b
Michal Tomcanyi <mtomcanyi@gmail.com>
2023-10-31 17:41:04 +0100

    UPDATE: Use variable-based config in profiles.yml

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ work_dir = f"{repository_path}/mi_postgres"
 os.chdir(work_dir)
 
 
+# Initialize environment variables
+os.environ["DBT_DEV_HOST"] = "changeme.meiro.io"
+os.environ["DBT_DEV_PORT"] = "5432"
+os.environ["DBT_DEV_USER"] = "changeme"
+os.environ["DBT_DEV_PASS"] = "changeme"
+os.environ["DBT_DEV_DBNAME"] = "changeme"
+os.environ["DBT_DEV_SCHEMA"] = "changeme"
+
+
 res: dbtRunnerResult = dbt.invoke(cli_args)
 
 if res.result != None:
